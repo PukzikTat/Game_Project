81cd1658cb6b14cbd417b331b028f28a776600d6
PukzikTat <Manmantattat123@gmail.com>
2026-08-10 22:39:10 -0700

    Adding some libraries and game start

--- Game.h
+++ Game.h
@@ -2,5 +2,9 @@
 using namespace std;
 
 class Game{
-
+public:
+    void start();
+private:
+    vector<Player> players;
+    vector<Enemy> enemies;
 };
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include "Game.h"
+#include "Player.h"
+#include "Enemy.h"
+
+int main(){
+    Game game;
+    game.start();
+    return 0;
+}
